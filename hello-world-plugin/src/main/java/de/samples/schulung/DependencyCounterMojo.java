096ba80fd15a2f3737ce61f8d83ef4b22c5c5f39
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-10-02 11:44:37 +0200

    replace project injection

--- hello-world-plugin/src/main/java/de/samples/schulung/DependencyCounterMojo.java
+++ hello-world-plugin/src/main/java/de/samples/schulung/DependencyCounterMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -17,11 +18,7 @@ import java.util.List;
 )
 public class DependencyCounterMojo extends AbstractMojo {
 
-    @Parameter(
-            defaultValue = "${project}",
-            required = true,
-            readonly = true
-    )
+    @Component
     MavenProject project;
 
     @Parameter(
