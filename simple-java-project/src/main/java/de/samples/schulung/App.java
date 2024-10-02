cd5a22549778a8b9a558a257b63e4395296b8fd8
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-10-02 11:57:26 +0200

    add dependency to module 3 in module 1

--- simple-java-project/src/main/java/de/samples/schulung/App.java
+++ simple-java-project/src/main/java/de/samples/schulung/App.java
@@ -13,5 +13,7 @@ public class App {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
         logger.debug(() -> "Hello World!");
+        Person person = new Person("Max");
+        logger.debug(person::sayHello);
     }
 }
